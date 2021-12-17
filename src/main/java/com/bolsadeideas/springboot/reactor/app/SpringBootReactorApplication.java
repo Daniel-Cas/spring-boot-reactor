6a62c8591616beead4d9402fd570714c33c8e4df
Daniel-Cas <dmattcomics@gmail.com>
2021-12-17 10:40:28 -0500

    Sectio 36 finished

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,11 +1,14 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import com.bolsadeideas.springboot.reactor.app.models.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
@@ -18,18 +21,21 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<String> names = Flux.just("Daniel","Pepe","Matthew","Michelle","Samira")
-                .doOnNext(e -> {
-                    if(e.isEmpty()){
+        Flux<Usuario> names = Flux.just("Daniel","Pepe","Matthew","Michelle","Samira")
+                .map( nombre -> new Usuario(nombre.toUpperCase(), null))
+                .doOnNext(usuario  -> {
+                    if(usuario == null){
                         throw  new RuntimeException("Nombres no pueden ser vacíos");
                     }
-                        System.out.println(e);
+                        System.out.println( usuario.getNombre() );
                 })
-                        .map( name -> {
-                            return name.toUpperCase();
+                        .map( usuario -> {
+                            String nombre =  usuario.getNombre().toLowerCase();
+                            usuario.setNombre(nombre);
+                            return usuario;
                         });
 
-        names.subscribe(response -> Log.info(response),
+        names.subscribe(response -> Log.info(response.toString()),
                 error -> Log.error(error.getMessage()),
                 new Runnable() {
                     @Override
